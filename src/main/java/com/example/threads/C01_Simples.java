cfefa3d4a2bf02db273af9c6df7ebf3615f34bff
marciofiladelfo <marcio.kabana@gmail.com>
2023-02-26 19:56:40 -0300

    Implementado aula threads

---
+++ src/main/java/com/example/threads/C01_Simples.java
@@ -0,0 +1,9 @@
+package com.example.threads;
+
+public class C01_Simples {
+
+    public static void main(String[] args) {
+        new Thread( () -> System.out.println("t-1") ).start();
+        System.out.println( "t2");
+    }
+}
